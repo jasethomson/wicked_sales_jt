1440d9daf42b0aa60cd32f29c6eda9df3490a231
jasethomson <jasethomson31@gmail.com>
2019-08-22 10:47:51 -0700

    Define a stateless  component in  that renders a single product card.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function ProductListItem(props) {
   return (
     <div className="card" style={{ width: '18rem' }}>
-      <img className="card-img-top" src="..." alt="Card image cap"></img>
+      <img className="card-img-top" src="images/la_galaxy.jpg" alt="Card image cap"></img>
       <div className="card-body">
         <h5 className="card-title">{props.product}</h5>
         <p className="card-text">{props.cost}</p>
