d882894e1f36c9f1e4df79121169c312c1d91787
jasethomson <jasethomson31@gmail.com>
2019-08-22 16:09:12 -0700

    Added http_response_code(500) to set status code correct, removed comments from code

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -5,11 +5,8 @@ function error_handler( $error ){
     "success" => false,
     "error" => $error->getMessage()
   ];
+  http_response_code(500);
   $json_output = json_encode($output);
   print($json_output);
 }
-
-
-
-
 ?>
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,8 +1,6 @@
 <?php
 header('Content-Type: application/json');
 require_once 'functions.php';
-http_response_code(500);
-
 set_exception_handler( 'error_handler' );
 
 $output = file_get_contents('dummy-products-list.json');
