6a87ac8b9496e64e42b9c9a06c90d4e34f9e4a92
jasethomson <jasethomson31@gmail.com>
2019-08-23 12:27:36 -0700

    Completed adding App's initial state with two props, catalog and params

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -4,7 +4,15 @@ import ProductList from './product-list';
 import ProductDetails from './product-details';
 
 export default class App extends React.Component {
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+  }
   render() {
     return (
       <div>
