2952131f032b5b9951e2b2fe49d198dc49300726
jasethomson <jasethomson31@gmail.com>
2019-08-23 12:24:13 -0700

    Completed adding ProductDetails conditional rendering of single product

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductListItem from './product-list-item';
 
 class ProductDetails extends React.Component {
   constructor(props) {
@@ -6,14 +7,35 @@ class ProductDetails extends React.Component {
     this.state = {
       product: null
     };
+    this.id = null;
+    this.name = null;
+    this.price = null;
+    this.image = null;
+    this.shortDescription = null;
+
   }
+
   componentDidMount() {
     fetch('/api/products.php?id=1')
       .then(res => res.json())
       .then(product => this.setState({ product }));
   }
+
   render() {
-    return null;
+    if (this.state.product) {
+      return (
+        <div className="d-flex">
+          <ProductListItem
+            key={this.state.product.id}
+            name={this.state.product.name}
+            price={this.state.product.price}
+            image={this.state.product.image}
+            shortDescription={this.state.product.shortDescription} />
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
