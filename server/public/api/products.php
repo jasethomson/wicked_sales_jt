262e6af6eb7869096a1b39d898a3110774d54bf7
jasethomson <jasethomson31@gmail.com>
2019-08-22 17:26:28 -0700

    Completed B02-connect-products-to-mysql

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,7 +3,10 @@ header('Content-Type: application/json');
 require_once 'functions.php';
 set_exception_handler( 'error_handler' );
 
+
+require_once 'db_connection.php';
 $output = file_get_contents('dummy-products-list.json');
 print( $output );
+
 
 ?>
