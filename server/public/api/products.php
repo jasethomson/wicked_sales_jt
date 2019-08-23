e5aa8b4fcb79ae345afa1eeac12c3791be527966
jasethomson <jasethomson31@gmail.com>
2019-08-23 07:18:47 -0700

    Completed (be_04-access-products-data)

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -3,10 +3,38 @@ header('Content-Type: application/json');
 require_once 'functions.php';
 set_exception_handler( 'error_handler' );
 
+startup();
 
 require_once 'db_connection.php';
-$output = file_get_contents('dummy-products-list.json');
-print( $output );
 
+
+$query = "SELECT * FROM `products`";
+$result = mysqli_query($conn, $query);
+
+if (!$result) {
+  print("Error: " . mysqli_error($conn));
+  exit();
+}
+
+$output = [
+  "success" => "true",
+  "data" => []
+];
+
+if (!(mysqli_num_rows($result))) {
+  print("no data available");
+  exit();
+}
+
+while ($row = mysqli_fetch_assoc($result)) {
+  $output['data'][] = $row;
+}
+
+$json_output = json_encode($output['data']);
+print($json_output);
+
+function startup(){
+  header("Content-type:application/json");
+}
 
 ?>
