e33f3c5e2f1220f0b58799b7386e7d1f0078e275
jasethomson <jasethomson31@gmail.com>
2019-08-24 16:00:58 -0700

    Completed be_05_adapt_products_details

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,22 +1,25 @@
 <?php
 
 require_once 'functions.php';
+require_once 'db_connection.php';
 set_exception_handler( 'error_handler' );
 startup();
 
 if (empty($_GET['id'])) {
   $whereClause = "";
-} else {
-  if(is_numeric($_GET['id'])){
+} elseif (is_numeric($_GET['id'])){
     $whereClause = "WHERE `id`=" . $_GET['id'];
   } else {
     throw new Exception("id needs to be a number" . mysqli_error($conn));
-  }
-}
+ }
 
-require_once 'db_connection.php';
 $query = "SELECT * FROM `products`" . $whereClause;
 $result = mysqli_query($conn, $query);
+
+$row_cnt = mysqli_num_rows($result);
+if($row_cnt === 0){
+  throw new Exception("Invalid ID: " . $_GET['id'] . mysqli_error($conn));
+}
 
 if (!$result) {
   throw new Exception("Connect Failed: " . mysqli_error($conn));
@@ -27,6 +30,7 @@ if (!(mysqli_num_rows($result))) {
   print($output);
   exit();
 }
+
 
 while ($row = mysqli_fetch_assoc($result)) {
   $output[] = $row;
