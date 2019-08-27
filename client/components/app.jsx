27e7ade12b9d020a919be621e45e7ed79f378d96
jasethomson <jasethomson31@gmail.com>
2019-08-27 13:16:15 -0700

    Mid add to cart, fixing Bootstrap grid system, almost there........

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,22 +10,31 @@ class App extends React.Component {
       view: {
         name: 'catalog',
         params: {}
-      }
+      },
+      cart: []
     };
     this.setView = this.setView.bind(this);
   }
   setView(name, params) {
     this.setState({ view: { name, params } });
   }
+  getCartItems() {
+    fetch('/api/cart.php')
+      .then(res => res.json())
+      .then(cart => this.setState({ cart }));
+  }
+  componentDidMount() {
+    this.getCartItems();
+  }
   render() {
     if (this.state.view.name === 'catalog') {
       return (
         <div>
-          <div className="headerContainer">
+          <div className="container">
             <Header text="Wicked Sales" />
           </div>
           <div className="container">
-            <div className="justify-content-center row">
+            <div className="row">
               <ProductList setView={this.setView} />
             </div>
           </div>
@@ -33,9 +42,9 @@ class App extends React.Component {
       );
     } else {
       return (
-        <div className="d-flex justify-content-center flex-column">
-          <div className="headerContainer">
-            <Header text="Wicked Sales" />
+        <div>
+          <div className="container">
+            <Header text="Wicked Sales" cartItemCount={this.state.cart}/>
           </div>
           <ProductDetails setView={this.setView} view={this.state.view.params}/>
         </div>
--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function Header(props) {
-
   return (
-    <div className="d-flex flex-column justify-content-center">
-      <img className="logo" src="images/ws_logo2.png"></img>
-      <h1 className="title">{props.text}</h1>
+    <div className="row">
+      <div className="col d-flex">
+        <img className="logo" src="images/ws_logo2.png"></img>
+        <h1 className="title">{props.text}</h1>
+      </div>
+      <div className="col">
+        <i className="fas fa-shopping-cart"></i>
+      </div>
     </div>
   );
 }
