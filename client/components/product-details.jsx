63bdf8358fc06a1fa3a760d22d2dd31c275550e3
jasethomson <jasethomson31@gmail.com>
2019-08-26 22:36:19 -0700

    Styled Details page with bootstrap to move closer towards the style of the figma

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -17,13 +17,12 @@ class ProductDetails extends React.Component {
       .then(res => res.json())
       .then(product => this.setState({ product }));
   }
-
   render() {
     if (this.state.product) {
       return (
         <div className="container">
           <div className="row">
-            <div className="col">
+            <div className="col-12">
               <button className="btn btn-link" onClick={() => { this.props.setView('catalog', {}); }}>Back To Catalog</button>
               <div className="card row">
                 <div className="card-body row">
@@ -32,10 +31,18 @@ class ProductDetails extends React.Component {
                   </div>
                   <div className="col-6">
                     <h3 className="card-title" onClick={() => this.props.setView('details', { id: this.state.product.id })}>{this.state.product.name}</h3>
-                    <p className="card-text" style={{ 'font-size': '1.5rem' }}>${((this.state.product.price / 100).toFixed(2))}</p>
-                    <p className="card-text" style={{ 'font-size': '1.4rem' }}>{this.state.product.shortDescription}</p>
+                    <p className="card-text" style={{ 'fontSize': '1.5rem' }}>${((this.state.product.price / 100).toFixed(2))}</p>
+                    <p className="card-text" style={{ 'fontSize': '1.4rem' }}>{this.state.product.shortDescription}</p>
                   </div>
-                  <p className="card-text col-12 mt-3" style={{ 'font-size': '1.2rem' }}>{this.state.product.longDescription}</p>
+                  <p className="card-text col-12 mt-3" style={{ 'fontSize': '1.2rem' }}>{this.state.product.longDescription.split('\n').map(function (item, key) {
+                    return (
+                      <span key={key}>
+                        {item}
+                        <br />
+                        <br />
+                      </span>
+                    );
+                  })}</p>
                 </div>
               </div>
             </div>
