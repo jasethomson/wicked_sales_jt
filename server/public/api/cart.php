6d935846ef03315a43c6f1d483e40e12b8c5ed85
jasethomson <jasethomson31@gmail.com>
2019-09-30 16:19:25 -0700

    Completed dump of data into database, commented out dummmy data for cart backend, working on functions for cart.php.

--- server/public/api/cart.php
+++ server/public/api/cart.php
@@ -1,21 +1,23 @@
 <?php
+//commented out code for dummy part
+// header('Content-Type: application/json');
 
-header('Content-Type: application/json');
+// $method = $_SERVER['REQUEST_METHOD'];
+// $item = file_get_contents('php://input');
 
-$method = $_SERVER['REQUEST_METHOD'];
-$item = file_get_contents('php://input');
+// if ($method == 'GET') {
+//   readfile('dummy-cart-items.json');
+// } else if ($method == 'POST') {
+//   http_response_code(201);
+//   print($item);
+// } else {
+//   http_response_code(404);
+//   print(json_encode([
+//     'error' => 'Not Found',
+//     'message' => "Cannot $method /api/cart.php"
+//   ]));
+// }
 
-if ($method == 'GET') {
-  readfile('dummy-cart-items.json');
-} else if ($method == 'POST') {
-  http_response_code(201);
-  print($item);
-} else {
-  http_response_code(404);
-  print(json_encode([
-    'error' => 'Not Found',
-    'message' => "Cannot $method /api/cart.php"
-  ]));
-}
+define("INTERNAL", true);
 
 ?>
