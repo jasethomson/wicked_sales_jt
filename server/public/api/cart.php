a4b7d21819a7aca3438d32229c610658e0fd45a6
jasethomson <jasethomson31@gmail.com>
2019-09-30 16:34:01 -0700

    Completed the session start, adding connections file, error handler and switch statement to check what type of request is coming in and require the file. Also created cart_add file.

--- server/public/api/cart.php
+++ server/public/api/cart.php
@@ -18,6 +18,23 @@
 //   ]));
 // }
 
+require_once 'functions.php';
+session_start();
+set_exception_handler('error_handler');
+startup();
+require_once 'db_connection.php';
+
 define("INTERNAL", true);
+
+switch($_SERVER['REQUEST_METHOD']){
+  case 'POST':
+    require_once 'cart_add.php';
+    break;
+  case 'GET':
+    require_once 'cart_get.php';
+    break;
+}
+
+
 
 ?>
