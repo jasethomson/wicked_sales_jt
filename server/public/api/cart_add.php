148b79aee231490a871787e5eaae98a7b5b2ae1e
jasethomson <jasethomson31@gmail.com>
2019-10-03 09:24:40 -0700

    Updated data

--- server/public/api/cart_add.php
+++ server/public/api/cart_add.php
@@ -27,12 +27,12 @@ $priceQuery = "SELECT `price` FROM `products` WHERE `id` = " . $cardID;
 $priceResult = mysqli_query($conn, $priceQuery);
 
 if(!$priceResult){
-  throw new Exception("Connect Failed: " . mysqli_error($conn));
+  throw new Exception("price result failed");
 }
 
 $row_cnt = mysqli_num_rows($priceResult);
-if(!empty($cartID) && $row_cnt === 0){
-  throw new Exception("Invalid ID: " . $cartID . mysqli_error($conn));
+if($row_cnt === 0){
+  throw new Exception("no price rows data came back");
 }
 
 $productData = [];
@@ -58,10 +58,25 @@ if($cartID === false){
     throw new Exception("affected rows is not equal to 1");
   }
   $cartID = mysqli_insert_id($conn);
-  $_SESSION['cartID']
+  $_SESSION['cartID'] = $cartID;
 }
 
 
-print($id);
+$insertToTableQuery = "INSERT INTO `cartItems` SET count=1, productID=$id,
+ price=$priceResult, added=NOW(), cartID=$cartID
+ ON DUPLICATE KEY UPDATE count=count + 1";
+
+$insertToTableResult = mysqli_query($conn, $insertToTableQuery);
+
+if(!$insertToTableResult){
+  throw new Exception("failed to get insert result");
+}
+
+if (mysql_affected_rows() !== 1) {
+  mysqli_query($conn, "ROLLBACK");
+  throw new Exception("affected rows is not equal to 1");
+}
+
+mysqli_query($conn, "COMMIT");
 
 ?>
