1f1ff3a58c3bcbbbe5259733a0724264d076c814
jasethomson <jasethomson31@gmail.com>
2019-10-11 09:37:01 -0700

    Added padding to cards to space them apart

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 function ProductListItem(props) {
 
   return (
-    <div className="bg-light col-3 mt-5 card d-flex my-2">
-      <div>
-        <img className="card-img-top mt-4" style={{ height: '180px' }}src={props.image} alt="Card image cap"></img>
-      </div>
-      <div>
-        <div className="card-body">
-          <h5 className="card-title" onClick={() => props.setView('details', { id: props.id })}>{props.name}</h5>
-          <p className="card-text">${((props.price / 100).toFixed(2))}</p>
-          <p className="card-text">{props.shortDescription}</p>
+    <div className="px-1 col-3 mt-5 d-flex my-2">
+      <div className="bg-light card col-12">
+        <div>
+          <img className="card-img-top mt-4" style={{ height: '180px' }} src={props.image} alt="Card image cap"></img>
         </div>
+        <div>
+          <div className="card-body">
+            <h5 className="card-title" onClick={() => props.setView('details', { id: props.id })}>{props.name}</h5>
+            <p className="card-text">${((props.price / 100).toFixed(2))}</p>
+            <p className="card-text">{props.shortDescription}</p>
+          </div>
+        </div>
+        <p className="card-text">{props.longDescription}</p>
       </div>
-      <p className="card-text">{props.longDescription}</p>
     </div>
   );
 }
