4f70cebe1b79251582653f69aaed25cbd45df85b
jasethomson <jasethomson31@gmail.com>
2019-10-21 13:21:10 -0700

    Added up and down arrow, with item count state and a function to increase the count of state by clicking either up or down.

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -5,7 +5,8 @@ class ProductDetails extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      product: null
+      product: null,
+      items: 1
     };
   }
   componentDidMount() {
@@ -13,6 +14,17 @@ class ProductDetails extends React.Component {
       .then(res => res.json())
       .then(product => this.setState({ product }));
   }
+
+  numOfItems(event) {
+    let itemsCopy = this.state.items;
+    if (event.currentTarget.id === 'up' && itemsCopy >= 1) {
+      itemsCopy += 1;
+    } else if (event.currentTarget.id === 'down' && itemsCopy > 1) {
+      itemsCopy -= 1;
+    }
+    this.setState({ items: itemsCopy });
+  }
+
   render() {
     if (this.state.product) {
       return (
@@ -29,6 +41,11 @@ class ProductDetails extends React.Component {
                     <h3 className="card-title" onClick={() => this.props.setView('details', { id: this.state.product[0].id })}>{this.state.product[0].name}</h3>
                     <p className="card-text" style={{ 'fontSize': '1.5rem' }}>${((this.state.product[0].price / 100).toFixed(2))}</p>
                     <p className="card-text" style={{ 'fontSize': '1.4rem' }}>{this.state.product[0].shortDescription}</p>
+                    <div className="numItems d-flex justify-content-start">
+                      <i id="up" className="fas fa-chevron-up" onClick={event => this.numOfItems(event)}></i>
+                      <div className="itemCount">{this.state.items}</div>
+                      <i id="down" className="fas fa-chevron-down" onClick={event => this.numOfItems(event)}></i>
+                    </div>
                     <button className="btn btn-outline-dark" onClick={() => this.props.addToCart(this.state.product[0])}>Add to Cart</button>
                   </div>
                   <p className="card-text col-12 mt-3" style={{ 'fontSize': '1.2rem' }}>{this.state.product[0].longDescription}</p>
