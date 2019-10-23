cabac4efcd41abc76b13e4dc0848329070f2dbcb
jasethomson <jasethomson31@gmail.com>
2019-10-22 18:14:21 -0700

    updated state and handleChanage for checkout form.

--- client/components/checkout-form.jsx
+++ client/components/checkout-form.jsx
@@ -16,10 +16,15 @@ class CheckoutForm extends React.Component {
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
-  handleChange() {
-    if (event.target.className === 'name') {
+  handleChange(event) {
+
+    if (event.target.id === 'firstName') {
       this.setState({
-        name: event.target.value
+        firstName: event.target.value
+      });
+    } else if (event.target.className === 'lastName') {
+      this.setState({
+        lastName: event.target.value
       });
     } else if (event.target.className === 'creditCard') {
       this.setState({
@@ -29,51 +34,69 @@ class CheckoutForm extends React.Component {
       this.setState({
         address: event.target.value
       });
+    } else if (event.target.className === 'address2') {
+      this.setState({
+        address2: event.target.value
+      });
+    } else if (event.target.className === 'city') {
+      this.setState({
+        city: event.target.value
+      });
+    } else if (event.target.className === 'state') {
+      this.setState({
+        state: event.target.value
+      });
+    } else if (event.target.className === 'zip') {
+      this.setState({
+        zip: event.target.value
+      });
     }
   }
+
   handleSubmit() {
     event.preventDefault();
   }
+
   render() {
     return (
-      <form className="mt-5">
+      <form onSubmit={this.handleSubmit} className="mt-5">
         <div className="form-row">
           <div className="col">
             <label className="text-white" htmlFor="inputAddress">First name</label>
-            <input type="text" className="form-control" placeholder="First name"></input>
+            <input id="firstName" type="text" className="form-control" placeholder="First name" onChange={this.handleChange}></input>
           </div>
           <div className="col mb-3">
             <label className="text-white" htmlFor="inputAddress">Last name</label>
-            <input type="text" className="form-control" placeholder="Last name"></input>
+            <input type="text" className="form-control" placeholder="Last name" onChange={this.handleChange}></input>
           </div>
         </div>
         <div className="form-group">
           <label className="text-white" htmlFor="inputZip">Credit Card</label>
-          <input type="text" className="form-control" id="creditCard"></input>
+          <input type="text" className="form-control" id="creditCard" onChange={this.handleChange}></input>
         </div>
         <div className="form-group">
           <label className="text-white" htmlFor="inputAddress">Address</label>
-          <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St"></input>
+          <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" onChange={this.handleChange}></input>
         </div>
         <div className="form-group">
           <label className="text-white" htmlFor="inputAddress2">Address 2</label>
-          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor"></input>
+          <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" onChange={this.handleChange}></input>
         </div>
         <div className="form-row">
           <div className="form-group col-md-6">
             <label className="text-white" htmlFor="inputCity">City</label>
-            <input type="text" className="form-control" id="inputCity"></input>
+            <input type="text" className="form-control" id="inputCity" onChange={this.handleChange}></input>
           </div>
           <div className="form-group col-md-4">
             <label className="text-white" htmlFor="inputState">State</label>
-            <input type="text" className="form-control" id="inputState"></input>
+            <input type="text" className="form-control" id="inputState" onChange={this.handleChange}></input>
           </div>
           <div className="form-group col-md-2">
             <label className="text-white" htmlFor="inputZip">Zip</label>
-            <input type="text" className="form-control" id="inputZip"></input>
+            <input type="text" className="form-control" id="inputZip" onChange={this.handleChange}></input>
           </div>
         </div>
-        <button type="submit" className="btn btn-primary" onSubmit={() => { this.props.placeOrder('catalog', {}); }}>Submit Order</button>
+        <button type="submit" className="btn btn-primary">Submit Order</button>
       </form>
 
     );
