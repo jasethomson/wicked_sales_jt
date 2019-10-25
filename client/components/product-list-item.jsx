24df1035a62bcd4c7b6d45b978263c170905f0b3
jasethomson <jasethomson31@gmail.com>
2019-10-25 12:22:58 -0700

    Made changes to styling on website.

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 function ProductListItem(props) {
 
   return (
-    <div className="px-1 col-xl-3 col-md-6 mt-5 d-flex my-2">
+    <div className="px-1 col-xl-3 col-md-6 mt-5 d-flex my-2 listwork">
       <div className="card col-12">
         <div>
-          <img className="card-img-top mt-4" style={{ height: '340px' }} src={props.image} alt="Card image cap"></img>
+          <img className="card-img-top mt-4 list-photo" style={{ height: '340px' }} src={props.image} alt="Card image cap"></img>
         </div>
         <div>
           <div className="card-body px-0 font">
-            <h5 className="card-title" onClick={() => props.setView('details', { id: props.id })}>{props.name}</h5>
-            <p className="card-text">${((props.price / 100).toFixed(2))}</p>
+            <h5 className="card-title list-name" onClick={() => props.setView('details', { id: props.id })}>{props.name}</h5>
+            <p className="card-text list-price">${((props.price / 100).toFixed(2))}</p>
             {/* <p className="card-text">{props.shortDescription}</p> */}
           </div>
         </div>
